5236110bfbd18f806bf082e7630302e179ee39a4
dughinho <marcospauloso2017@gmail.com>
2025-06-01 07:24:45 -0300

    editei algo

--- Mensagem.c
+++ Mensagem.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main()
 {
-	printf("NOVA MENSAGEM INSERIDA!");
-	
+	int variavel_qualquer = 0;
+		printf("oi, digita algo: ");
+		scanf("%d", &variavel_qualquer);
+			printf("%d", variavel_qualquer);
+	///teste
 	return 0;
 }
